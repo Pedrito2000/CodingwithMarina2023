168289c2c1982da1564ce7d01a3ed13b3c7b25eb
Pedro <Darthrevanmalak2000@gmail.com>
2023-05-29 16:20:11 +0200

    foto

---
+++ src/UF5/Practica1/Main.java
@@ -0,0 +1,10 @@
+package UF5.Practica1;
+
+public class Main {
+    public static void main(String[] args) {
+        Ventana V1 = new Ventana();
+        V1.setVisible(true);
+
+
+    }
+}
---
+++ src/UF5/Practica1/Ventana.java
@@ -0,0 +1,34 @@
+package UF5.Practica1;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Ventana extends JFrame {
+    public Ventana (){
+        setSize(500,500);
+        iniciarcomponentes();
+    }
+
+    public void iniciarcomponentes(){
+        //Creamos el panel
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        this.getContentPane() .add(panel);
+
+        //Creamos los botones
+        //Boton 1
+        JButton boton1 = new JButton("Click");
+        boton1.setBounds(100,100,100,40);
+        boton1.setForeground(Color.BLUE);
+        boton1.setFont(new Font("cooper black",3,20));
+        panel.add(boton1);
+
+        //Boton2
+        JButton boton2 = new JButton();
+        boton2.setBounds(100,200,100,40);
+        boton2.setBackground(Color.BLUE);
+        panel.add(boton2);
+    }
+
+}
